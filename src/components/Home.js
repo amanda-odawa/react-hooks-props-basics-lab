96a76303095b6fbc5b22e62e59c1965798213d92
AmandaOdawa <amandaodawa1@gmail.com>
2025-01-21 10:12:34 +0300

    complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import user from "../data/user";
 
-function Home(props) {
+function Home({ name = user.name, city = user.city, color = user.color }) {
   return (
     <div id="home">
-      <h1 style={{ color: props.color }}>
-        {props.name} is a Web Developer from {props.city}
+      <h1 style={{ color }}>
+        {name} is a Web Developer from {city}
       </h1>
     </div>
   );
